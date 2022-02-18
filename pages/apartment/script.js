08cf2e81ddf63b711644cbf4f4fc7cdb68fda65a
Hovhannes <hovnaz.arm@gmail.com>
2022-02-18 13:26:56 +0400

    add Cookie and Storage

--- pages/apartment/script.js
+++ pages/apartment/script.js
@@ -1,29 +1,58 @@
-function changeData(update=false){
-    if (update) {
-        const all = $("#header-baner [data-name]");
-        for (const item of all) {
-            const name = $(item).data("name");
-            $(`[data-name="${name}"]`).text($(item).text());
-            $(`[data-name="${name}"]`).val($(item).text());
-            // console.log($(`[data-name="${name}"]`));
+let dataStorage = {
+    aptNumber: "poghos",
+    floor: "petros",
+    rooms: "mardiros",
+    parking: "hmayak",
+    location: "Gyumri",
+    completedDate: "45ff67",
+}
+function changeData(storageU=false){
+    const input = $("input[data-name]");
+    main:
+    for (const key in dataStorage) {
+        const storage = localStorage.getItem(key);
+        const cookie = getCookie(key);
+        let value = dataStorage[key];
+        let sel = `[data-name="${key}"]`;
+        if (storageU == "input"){
+            for (const inputItem of input) {
+                let value = $(inputItem).val();
+                let name = $(inputItem).data("name");
+                $(`[data-name="${name}"]`).text(value);
+                dataStorage[name] = value;
+            }
+            break main;
         }
-        return;
-    }
-    const items = $("[data-name]").not("input");
-    for (const item of items) {
-        const name = $(item).data("name");
-        const input = $(`input[data-name="${name}"]`);
-        if (input) {
-            $(item).text(input.val());
+        else if (storageU == "Storage" && (storage || storage == "")) {
+            value = storage;
+            dataStorage[key] = storage;
         }
+        else if (storageU == "Cookie" && (cookie || cookie == "")) {
+            value = cookie;
+            dataStorage[key] = cookie;
+        }
+        else sel = ":not(input)"+sel;
+        $(sel).text(value).val(value);
     }
 }
 
+function setStorage() {
+    for (const key in dataStorage) {
+        localStorage.setItem(key, dataStorage[key])
+    }
+}
+function setCookiE() {
+    for (const key in dataStorage) {
+        setCookie(key,dataStorage[key]);
+    }
+}
 
-$( "#popup-apartment-click" ).click(function() {
-    changeData(true);
-});
-
-// $( "[data-name]" ).change(function() {
-//     alert( "Handler for .change() called." );
-// });
+window.onload = function (e) {
+    changeData("Storage");
+    // changeData("Cookie");
+};
+window.onbeforeunload = function (e) {
+    setStorage();
+    // setCookiE();
+    changeData();
+};
